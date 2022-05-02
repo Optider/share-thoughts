3c427f1dd1f72f6975b562e773878a86850b1f5a
Anuj Sharma <anujsharmasppu@gmail.com>
2022-05-02 11:15:20 +0530

    enforce HTTPS

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ connectDB()
 
 const app = express()
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 // Body parser
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
